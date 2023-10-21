811f39cfe95f738131bc7a9ab2ca3d71046e074c
nananagen <laura.augusto.pb@compasso.com.br>
2023-10-21 11:44:13 -0300

    alguns detalhes

--- projeto-prototipodbs/my-prototype/src/App.tsx
+++ projeto-prototipodbs/my-prototype/src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
-import Planilha from '../src/components/Planilha';
-import 'handsontable/dist/handsontable.full.css'; // Importar os estilos do Handsontable
+import Planilha from './components/Planilha';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Protótipo de Planilha - Challenge DBS</h1>
-        <Planilha />
+        <h1>Protótipo de Planilha</h1>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link id='link' to="/login">Login</Link>
+              </li>
+              <li>
+                <Link id='link' to="/planilha">Planilha</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/planilha" element={<Planilha />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
---
+++ projeto-prototipodbs/my-prototype/src/components/Login.tsx
@@ -0,0 +1,36 @@
+// Login.tsx
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (username === '123' && password === '123') {
+      navigate('/planilha');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Nome de usuário"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Entrar</button>
+    </div>
+  );
+};
+
+export default Login;
